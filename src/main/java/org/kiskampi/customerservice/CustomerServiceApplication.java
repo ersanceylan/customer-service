05c930ada0cb4bc2b25df68860b5660bc6f5ad3d
Ersan Ceylan <ersan@kodgemisi.com>
2019-02-10 14:21:21 +0300

    initial

---
+++ src/main/java/org/kiskampi/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,28 @@
+package org.kiskampi.customerservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableEurekaClient
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+	@Value("${test}")
+	private String test;
+
+	@GetMapping("/show-property")
+	public String showPropertySource() {
+		return test;
+	}
+
+}
+
